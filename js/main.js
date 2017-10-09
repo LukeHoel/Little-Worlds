03a6221ddcb20704ab70b14c5bc6c6959cace57c
LukeHoel <hoel.luke@gmail.com>
2017-10-09 21:52:44 +0200

    added ticker and removed all uneeded code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-createjs.Ticker.on("tick", update);
-
+var stage;
 function init() {
-    var stage = new createjs.Stage("canvas");
-    stage.update();    
+    stage = new createjs.Stage("canvas");
+    stage.update();     
+    createjs.Ticker.on("tick", update);
 }
 
 function update (event){
     //run everything in here!
     if (!event.paused) {
-        // Actions carried out when the Ticker is not paused.
+        // Actions carried out when the Ticker is not paused
         stage.update();    
     }
 }
