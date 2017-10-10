25cc9e0208fa7a9c47b4062d656edd8efdbc0b17
LukeHoel <hoel.luke@gmail.com>
2017-10-10 11:49:19 +0200

    added basic responsiveness to canvas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,48 @@
 var stage;
+var planet;
 function init() {
+    var canvas = document.getElementsByTagName('canvas')[0];
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    window.addEventListener('resize', resize, false);    
+
     stage = new createjs.Stage("canvas");
-    stage.update();     
-    createjs.Ticker.on("tick", update);
+
+    planet = new createjs.Shape();
+    planet.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 100);
+    planet.x = window.innerWidth/2;
+    planet.y = window.innerHeight/2;
+    stage.addChild(planet);
+
+    stage.update();
+
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", function (e) {
+        update(e);
+    });
+
 }
 
-function update (event){
+function update(e) {
     //run everything in here!
-    if (!event.paused) {
+    if (!e.paused) {
         // Actions carried out when the Ticker is not paused
-        stage.update();    
+        stage.update();
     }
+}
+
+function resize(){
+    var canvas = document.getElementsByTagName('canvas')[0];
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function zoomIn(){
+    stage.scaleX += .1;
+    stage.scaleY += .1;
+}
+function zoomOut(){
+    stage.scaleX += .1;
+    stage.scaleY += .1;
 }
