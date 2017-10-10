ea22c5eada5f24722ba4702c1ee8062b6364b9e9
LukeHoel <hoel.luke@gmail.com>
2017-10-10 12:59:59 +0200

    added basic controls to planet

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 var stage;
 var planet;
+var scale = 1;
+var upKey = false;
+var downKey = false;
+var leftKey = false;
+var rightKey = false;
+
+function debug() {
+    debugger;
+}
+
 function init() {
     var canvas = document.getElementsByTagName('canvas')[0];
-    canvas.width  = window.innerWidth;
+    canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    window.addEventListener('resize', resize, false);    
+    window.addEventListener('resize', resize, false);
 
     stage = new createjs.Stage("canvas");
 
     planet = new createjs.Shape();
     planet.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 100);
-    planet.x = window.innerWidth/2;
-    planet.y = window.innerHeight/2;
+    planet.x = window.innerWidth / 2;
+    planet.y = window.innerHeight / 2;
+    planetX = planet.x;
+    planetY = planet.y;
     stage.addChild(planet);
 
     stage.update();
@@ -24,25 +36,87 @@ function init() {
 
 }
 
+function resize() {
+    var canvas = document.getElementsByTagName('canvas')[0];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
 function update(e) {
     //run everything in here!
     if (!e.paused) {
         // Actions carried out when the Ticker is not paused
+        if (planet.scaleX > scale + .03)
+            planet.scaleX -= .03;
+        else if (planet.scaleX < scale - .03)
+            planet.scaleX += .03;
+
+        if (planet.scaleY > scale + .03)
+            planet.scaleY -= .03;
+        else if (planet.scaleY < scale - .03)
+            planet.scaleY += .03;
+        
+        if(upKey)
+            planet.y -= 5;
+        else if(downKey)
+            planet.y += 5;
+        if(leftKey)
+            planet.x -= 5;
+        else if(rightKey)
+            planet.x += 5;
         stage.update();
     }
 }
 
-function resize(){
-    var canvas = document.getElementsByTagName('canvas')[0];
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+window.onkeyup = function (e) {
+    var key = e.keyCode ? e.keyCode : e.which;
 
-function zoomIn(){
-    stage.scaleX += .1;
-    stage.scaleY += .1;
+    switch (key) {
+        case (187):
+            zoomIn();
+            break;
+        case (189):
+            zoomOut();
+            break;
+        case (87):
+            upKey = false;
+            break;
+        case (83):
+            downKey = false;
+            break;
+        case (65):
+            leftKey = false;
+            break;
+        case (68):
+            rightKey = false;
+            break;
+    }
 }
-function zoomOut(){
-    stage.scaleX += .1;
-    stage.scaleY += .1;
+window.onkeydown = function (e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+
+    switch (key) {
+        case (87):
+            upKey = true;
+            break;
+        case (83):
+            downKey = true;
+            break;
+        case (65):
+            leftKey = true;
+            break;
+        case (68):
+            rightKey = true;
+            break;
+    }
+}
+function zoomIn() {
+    scale += .5;
+    stage.update();
+}
+function zoomOut() {
+    if (scale > 1) {
+        scale -= .5;
+        stage.update();
+    }
 }
